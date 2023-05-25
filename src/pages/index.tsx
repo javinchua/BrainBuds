794bc124a3a67621ae4c564805c727128452961a
javinchua <javin.chua@gmail.com>
2023-05-25 13:17:46 +0800

    shifted files out

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Layout from 'components/Layout'
+import { NextPage } from 'next'
+
+const IndexPage: NextPage = () => {
+  return (
+    <Layout>
+      <div className="fixed bottom-0 left-0 right-0">Hello</div>
+    </Layout>
+  )
+}
+
+export default IndexPage
