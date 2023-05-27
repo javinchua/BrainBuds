5f245389829a9d793aa52eaa33df655cbe780b5b
javinchua <javin.chua@gmail.com>
2023-05-27 17:38:55 +0800

    fix responsiveness

--- src/components/SignupForm/index.tsx
+++ src/components/SignupForm/index.tsx
@@ -70,7 +70,7 @@ export const SignupForm: React.FC = () => {
   }
   return (
     <section className="bg-gray-900">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div className="flex flex-col items-center justify-center h-screen px-8 py-8 mx-auto lg:py-0">
         <div className="w-full bg-gray-800 border border-gray-700 rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl">
