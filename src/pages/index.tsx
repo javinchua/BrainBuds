0e58f7ab904111bceac00b98e55b85d499007718
javinchua <javin.chua@gmail.com>
2023-05-27 16:08:39 +0800

    removed layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,7 @@
-import Layout from 'components/Layout'
 import { NextPage } from 'next'
 
 const IndexPage: NextPage = () => {
-  return (
-    <Layout>
-      <div className="fixed bottom-0 left-0 right-0">Hello</div>
-    </Layout>
-  )
+  return <div className="fixed bottom-0 left-0 right-0">Hello</div>
 }
 
 export default IndexPage
